fa4ed83000c8c68c3463ebdec3c81359fab0cc2a
Reda <saada.reda@outlook.com>
2024-07-04 13:18:29 +0200

    non working s10

---
+++ S10 - Advanced State Management/01-starting-project/src/components/Shop.jsx
@@ -0,0 +1,11 @@
+export default function Shop({children}) {
+    return (
+        <section id="shop">
+            <h2>Elegant Clothing For Everyone</h2>
+
+            <ul id="products">
+                {children}
+            </ul>
+        </section>
+    );
+}
